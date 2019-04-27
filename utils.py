29626e61f964c34ebb2bccf4c7f0487246e2de92
quanbing <quanbinks@sina.com>
2019-04-27 13:51:09 +0800

    add functions

--- utils.py
+++ utils.py
@@ -11,11 +11,31 @@ import json
 import os
 
 
-def write_txt():
-    pass
+def write_txt(filename='test.csv', info='', mode='a'):
+    '''
+    写入txt文本文档
+    参数：
+        filename：文件路径
+        info：写入信息
+        mode:
+            r 只能读
+            r+ 可读可写 不会创建不存在的文件 从顶部开始写 会覆盖之前此位置的内容
+            w+ 可读可写 如果文件存在 则覆盖整个文件不存在则创建
+            w 只能写 覆盖整个文件 不存在则创建
+            a 只能写 从文件底部添加内容 不存在则创建
+            a+ 可读可写 从文件顶部读取内容 从文件底部添加内容 不存在则创建
+    '''
+    f = open(filename, mode=mode + r"\n")
+    try: f.writelines(info)
+    finally: f.close()
 
 
 def load_hfsd(abs_file_path, file_name):
+    '''
+    :param abs_file_path:
+    :param file_name:
+    :return: a list
+    '''
     return [json.load(open(os.path.join(abs_file_path, file_name), encoding='utf8'))]
 
 
@@ -23,7 +43,7 @@ def loads_hfsd(abs_file_path, file_name):
     '''
     :param abs_file_path:
     :param file_name:
-    :return:
+    :return: a dict
     '''
     result = []
     with open(os.path.join(abs_file_path, file_name), encoding='utf8') as f:
@@ -32,16 +52,17 @@ def loads_hfsd(abs_file_path, file_name):
     return result
 
 
-def check_parme_wrapper(args):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            param_num = func.__code__.co_argcount
-            param_list = func.__code__.co_varnames
-            for arg in args:
-                if arg in param_list:
-                    pass
-        return wrapper
-    return decorator
+def singleton(cls):
+    '''
+    :param cls: a derector to implement singleton cls
+    '''
+    _instance = {}
 
+    def _singleton(*args, **kargs):
+        if cls not in _instance:
+            _instance[cls] = cls(*args, **kargs)
+            return _instance[cls]
+        return _instance[cls]
+    return _singleton
 
 
