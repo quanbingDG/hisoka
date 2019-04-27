672c084e079f0a0ac580f353cbf2b7c30e725158
quanbing <quanbinks@sina.com>
2019-04-27 13:38:52 +0800

    add constent file

---
+++ const.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@author: quanbing
+@file: const.py
+@time: 2019-04-27 13:21
+"""
+
+HOSTNAME = '192.168.234.128'
+USERNAME = 'root'
+PASSWD = 'root'
+DBNAME = 'r'
