27f6a825cad9592a9ddb5033e062c21f9bc21166
quanbing <quanbinks@sina.com>
2019-04-27 13:41:20 +0800

    add origin_engin

--- ext.py
+++ ext.py
@@ -6,3 +6,20 @@
 @file: ext.py
 @time: 2019-04-11 17:24
 """
+
+
+import pymysql
+from const import HOSTNAME, USERNAME, PASSWD, DBNAME
+from utils import singleton
+
+
+@singleton
+class create_orgin_engine():
+    '''create mysql engine'''
+    def __init__(self):
+        self._engin = pymysql.connect(HOSTNAME, USERNAME, PASSWD, DBNAME)
+
+    @property
+    def engin(self):
+        return self._engin
+
