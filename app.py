ca44b219839e2c682f508906a645967fe60b96a4
quanbing <quanbinks@sina.com>
2019-04-28 22:07:12 +0800

    add login module

--- app.py
+++ app.py
@@ -8,10 +8,12 @@
 """
 
 
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
+from flask.views import MethodView
 import mock
 import upload_data
 from ext import db
+from utils import get_host_ip
 
 
 app = Flask(__name__, static_folder='./static', template_folder='./template')
@@ -21,12 +23,12 @@ db.init_app(app)
 
 @app.route('/index/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index_old.html', ip=get_host_ip())
 
 
 @app.route('/', methods=['GET'])
 def _index():
-    return redirect(url_for('index'))
+    return redirect(url_for('login_view'))
 
 
 @app.errorhandler(404)
@@ -34,6 +36,15 @@ def not_found(error):
     return render_template('mock_error.html'),404
 
 
+class UserAPI(MethodView):
+    def get(self):
+        return render_template("login.html")
+    def post(self):
+        if request.form['username'] == 'quanbing' and request.form['password'] == '123':
+            return render_template('index_old.html', ip=get_host_ip(), user='quanbing')
+        return "need login"
+
+app.add_url_rule('/login', view_func=UserAPI.as_view('login_view'))
 app.register_blueprint(mock.bp)
 app.register_blueprint(upload_data.bp)
 
